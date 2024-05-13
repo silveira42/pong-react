f86d637989d6b04c865d9375aabef4c5b5e820be
Bruno Silveira <silverdev42@gmail.com>
2024-05-13 19:49:58 -0300

    Small adjustments to header style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,13 +5,15 @@ import { HeaderProps } from './HeaderProps';
 export default function Header(props: HeaderProps) {
 	return (
 		<div
-			className='game-header-container'
+			className='header-container'
 			style={{ height: props.height, width: props.width }}
 		>
-			<div className='score-container'>
+			<div className='header-wrapper'>
 				<h2 className='score'>
 					Game score {props.score.player} - {props.score.opponent}
 				</h2>
+			</div>
+			<div className='header-wrapper'>
 				{props.showMatchScore ? (
 					<h2 className='score'>
 						Matches score {props.matchScore.player} -{' '}
